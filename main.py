1f8aeb1dda344e19b90757d8896a497985eebdec
Pakorn Apirakkit <pakoa457@cubes.postbox.in.th>
2023-09-16 15:59:09 +0700

    finn audio static generate

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import pyttsx3
 import os
-engine = pyttsx3.init()
 
-TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
 
-engine.setProperty('volume', 0.9)  # Volume 0-1
-engine.setProperty('rate', 130)  #148
 
-engine.setProperty('voice', TH_voice_id)
-# name = "ดร. สุวัจชัย"
-# emotion = "อารมณ์ดีนะ"
-# speech = f"สวัสดี คุณ {name} วันนี้ คุณดู{emotion}"
-speech = "ไอเก่ง เขาไม่รักมึงหรอก ไอโง่"
-engine.say(speech)
-# engine.save_to_file(speech, './new_speech/speech.mp3')
-
-engine.runAndWait()
 
 
 # map id to get nameTh
@@ -23,11 +10,66 @@ engine.runAndWait()
 # filename as id_nameTH_emotionEN
 
 
-arr_raw = os.listdir("./database")
-for v in arr_raw:
-    print(v)
-    x = v.split("_")
-    print(x)
-def Generate_AudioFile(name: str , emotion : str) -> str:
-    filepath = ""
-    return filepath
+
+# Generate for 1 Person get all emotion
+def Generate_AudioFile(id: str,name_EN: str,name_TH:str):
+    engine = pyttsx3.init()
+    TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
+
+    emotion_en = ["angry" , "disgust", "fear" , "happy", "sad" , "suprise", "neutral"]
+    emotion_th = ["คุณโกรธอะไรมาหรือป่าว" , "สีหน้าของคุณดูไม่ค่อยดีเลยนะ" , "คุณดูตื่นกลัวนะ" , "คุณดูมีความสุขมากนะ", "คุณเป็นอะไรหรือเปล่า ดูเศร้าๆนะ", "คุณดูตกใจนะ", "ขอให้เป็นวันที่ดีนะ"]
+    engine.setProperty('volume', 0.9)  # Volume 0-1
+    engine.setProperty('rate', 120)  #148
+
+    engine.setProperty('voice', TH_voice_id)
+    # name = "ดร. สุวัจชัย"
+    # emotion = "อารมณ์ดีนะ"
+    # name_th = []
+    dirname = f"./new_speech/{name_EN}"
+    if os.path.exists(dirname):
+        os.rmdir(dirname)
+    os.mkdir(f"./new_speech/{name_EN}")
+    for i ,v in enumerate(emotion_th):
+        speech = f"สวัสดี คุณ{name_TH} วันนี้ {v}"
+        filepath = f"./new_speech/{name_EN}/{id}_{emotion_en[i]}.mp3"
+        engine.save_to_file(speech, filepath)
+        # engine.say(speech)
+        print(f"{i} || {v}")
+
+    engine.runAndWait()
+    return
+
+if __name__ == "__main__":
+    name_dict = {
+        "25" : "เสาวนี",
+        "26" : "กมลรัตน์",
+        "27" : "กาญศายา",
+        "28" : "ณิชาภา",
+        "29" : "ทักศินา",
+        "30" : "ธนภร",
+        "31" : "พัททพล",
+        "32" : "มิรา",
+        "33" : "สุมิตรา",
+        "34" : "เกื้อกูล",
+        "35" : "จิระวัต",
+        "36" : "ธราดล",
+        "38" : "พลฉัตร",
+        "40" : "พิระพัก",
+        "42" : "ดร. นิกรณ์",
+        "43" : "ดร. ลือพล",
+        "44" : "ดร. สุวัจชัย",
+        "45" : "กรพิสิท",
+        "48" : "ธวัชชัย",
+        "49" : "พัทรพูม",
+    }
+    arr_raw = os.listdir("./database")
+    for v in arr_raw:
+        x = v.split("_")
+        id = x[0]
+        name = x[1]
+        if id not in name_dict:
+            continue
+        Generate_AudioFile(id,name_EN=name,name_TH=name_dict[id])
+
+        
+    
