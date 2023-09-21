6115011df27d1f3d244bff8f171cfd93c15fb449
Pakorn Apirakkit <pakoa457@cubes.postbox.in.th>
2023-09-21 17:10:15 +0700

    add default audio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import os
+import shutil
 
 # map id to get nameTh
 # generate with all emotion
@@ -12,23 +13,35 @@ def Generate_AudioFile(id: str,name_EN: str,name_TH:str):
     emotion_en = ["angry" , "disgust", "fear" , "happy", "sad" , "suprise", "neutral"]
     emotion_th = ["คุณโกรธอะไรมาหรือป่าว" , "สีหน้าของคุณดูไม่ค่อยดีเลยนะ" , "คุณดูตื่นกลัวนะ" , "คุณดูมีความสุขมากนะ", "คุณเป็นอะไรหรือเปล่า ดูเศร้าๆนะ", "คุณดูตกใจนะ", "ขอให้เป็นวันที่ดีนะ"]
     engine.setProperty('volume', 0.9)  # Volume 0-1
-    engine.setProperty('rate', 120)  #148
+    engine.setProperty('rate', 130)  #148
 
     engine.setProperty('voice', TH_voice_id)
-    # name = "ดร. สุวัจชัย"
-    # emotion = "อารมณ์ดีนะ"
-    # name_th = []
     dirname = f"./new_speech/{id}_{name_EN}"
     if os.path.exists(dirname):
-        os.rmdir(dirname)
+        shutil.rmtree(dirname)
     os.mkdir(dirname)
     for i ,v in enumerate(emotion_th):
         speech = f"สวัสดี คุณ{name_TH} วันนี้ {v}"
         filepath = f"{dirname}/{emotion_en[i]}.mp3"
         engine.save_to_file(speech, filepath)
-        # engine.say(speech)
-        print(f"{i} || {v}")
+    engine.runAndWait()
+    return
 
+def GenerateDefaultAudio():
+    engine = pyttsx3.init()
+    TH_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_THAI"
+    engine.setProperty('volume', 0.9)  # Volume 0-1
+    engine.setProperty('rate', 130)  #148
+    engine.setProperty('voice', TH_voice_id)
+    dirname = f"./new_speech/default"
+    if os.path.exists(dirname):
+         shutil.rmtree(dirname)
+    os.mkdir(dirname)
+    default_audio = ["คุณอุณหภูมิสูงกว่าปกติ" , "คุณอุณหภูมิปกติ", "ไม่สามารถประมวลผลได้"]
+    default_name = ["mask_high", "normal_mask", "notprocess"]
+    for i , speech in enumerate(default_audio):
+        filepath = f"{dirname}/{default_name[i]}.mp3"
+        engine.save_to_file(speech, filepath)
     engine.runAndWait()
     return
 
@@ -53,7 +66,7 @@ if __name__ == "__main__":
         "44" : "ดร. สุวัจชัย",
         "45" : "กรพิสิท",
         "48" : "ธวัชชัย",
-        "49" : "พัทรพูม",
+        "49" : "พัทระพูม",
     }
     arr_raw = os.listdir("./database")
     for v in arr_raw:
@@ -63,3 +76,4 @@ if __name__ == "__main__":
         if id not in name_dict:
             continue
         Generate_AudioFile(id = id,name_EN=name,name_TH=name_dict[id])
+    GenerateDefaultAudio()
